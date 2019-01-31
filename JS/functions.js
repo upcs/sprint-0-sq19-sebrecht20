57cf496c8fb5d67ad1715f249127cc2a84549fd5
sebrecht20 <35584175+sebrecht20@users.noreply.github.com>
2019-01-30 20:20:12 -0800

    Sprint-0_phase1

---
+++ JS/functions.js
@@ -0,0 +1,6 @@
+//Functions for html file written in javascript
+function addstwo(x,y)
+{
+return x+y;
+
+}
---
+++ JS/index.js
@@ -0,0 +1,5 @@
+//Chris Sebrechts
+//This is the function in function.js
+// feeds the info into index.html
+var total = addstwo(24,1);
+document.getElementById("header1").innerHTML = "Spongebob you know whats funnier than 24... " + total;
